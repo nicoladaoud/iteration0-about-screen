2eac5f73e21f95cf2b9cee44844759708dad279a
GyubeomKim <GyubeomKim@10.0.0.39>
2020-05-02 15:12:19 -0700

    Add GUI to java code

--- AboutBox.java
+++ AboutBox.java
@@ -1,26 +1,122 @@
-package information;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+/**
+ * .
+ * 
+ * @author WildCats
+ * @version 1.0
+ */
 
 public class AboutBox {
+    
+    /**
+     * ...
+     */
+    private static final String DEVELOPERS 
+                    = "Tyler Phippen, Nicola Daoud, Gyubeom Kim, and Jun Kim";
+    
+    /**
+     * set up Frame.
+     */
+    private final JFrame myMain;
+    
+    /**
+     * ...
+     */
+    private double myVersionNumb;
+    
+    /**
+     * ...
+     */
+    public AboutBox() {
+        this.myMain = new JFrame("Iteration0-Demo");
+        this.setVersionNumb(1.0);
+    }
+    
+    /**
+     * ...
+     */
+    public void start() {
+        
+        myMain.setSize(500, 500);
+        myMain.setLayout(null);
+        myMain.setVisible(true);
+        centerWindow(myMain);
+        myMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // panel
+        final JPanel panel = new JPanel();
+        panel.setBounds(0, 0, 500, 500);
+        panel.setLayout(null);
+        myMain.add(panel);
+        
+        // label and Button 
+        final JLabel label1 = new JLabel("Version: WildCats" + getVersionNumb());
+        final JButton button1 = new JButton("About");
+        label1.setBounds(180, 150, 95, 30);
+        button1.setBounds(200, 200, 95, 30);
+        label1.setSize(label1.getPreferredSize());
+        panel.add(button1);
+        panel.add(label1);        
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent theEvent) {
+                JOptionPane.showMessageDialog(null, DEVELOPERS, 
+                                              "Developers", JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+    }
+    
+    /**
+     * ...
+     * @return ...
+     */
+    public double getVersionNumb() {
+        return myVersionNumb;
+    }
+    
+    /**
+     * ...
+     * @param theVersionNumb ...
+     */
+    public void setVersionNumb(final double theVersionNumb) {
+        this.myVersionNumb = theVersionNumb;
+    }
+    
+    /**
+     * Main method.
+     * 
+     * @param theArgs arguments.
+     */
+    public static void main(final String[] theArgs) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
 
-	private final static String DEVELOPERS = "Tyler Phippen, Nicola Daoud, Gyubeom Kim, and Jun Kim";
-	private final static String VERSION_NUMBER = AboutBox.class.getPackage().getImplementationVersion();
-
-	public AboutBox() {
-
-	}
-
-	public String getMyVersionNumber() {
-		return VERSION_NUMBER;
-	}
-
-	public String getMyDevelopers() {
-		return DEVELOPERS;
-	}
-
-	public static void main(final String[] args) {
-		final AboutBox box = new AboutBox();
-		System.out.println(box.getMyDevelopers());
-		System.out.println(box.getMyVersionNumber());
-	}
-
+                final AboutBox mainFrame = new AboutBox();
+                mainFrame.start();
+            }
+        });
+    }
+    
+    /**
+     * .
+     * @param theFrame ...
+     */
+    private void centerWindow(final Window theFrame) {
+        final Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        final int x = (int) ((dimension.getWidth() - theFrame.getWidth()) / 2);
+        final int y = (int) ((dimension.getHeight() - theFrame.getHeight()) / 2);
+        theFrame.setLocation(x, y);
+    }
 }
